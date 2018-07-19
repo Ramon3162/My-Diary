e4fc87fc5380aa315635ea170ad59b30e975d51a
Ian Otieno <ramonomondi@gmail.com>
2018-07-19 22:20:58 +0300

    Change the base tests to run all the test case

--- tests/base.py
+++ tests/base.py
@@ -1,18 +1,19 @@
 """Creating the base class for all the other tests"""
 import unittest
 import json
+from unittest import TestCase
 from app import app
 
 
-class BaseTestClass():
+class BaseTestClass(TestCase):
     """Configuring the base test class for all the test cases"""
 
-    
+    @classmethod
     def setUp(self):
         """App configuration setup"""
         
         self.app = app
-        self.client = app.test_client()
+        self.app = self.app.test_client()
 
         self.entry_contents = {
             'title' : 'Trip to oblivion',
@@ -27,9 +28,8 @@ class BaseTestClass():
             'title' : 'You are in for a treat'
         }
 
-    
-    def classTearDown(self):
+    @classmethod
+    def classTearDown(cls):
         pass
 
-if __name__ == '__main__':
-    unittest.main()
+
