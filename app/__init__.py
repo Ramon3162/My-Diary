1328a1a5a6a932a26e36d0d7982f88a1ed159c00
Ian Otieno <ramonomondi@gmail.com>
2018-07-24 23:17:47 +0300

    Update the init file with the signup and login endpoinst

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 
+
 #List to hold the Entry data
 
 entries = [
@@ -22,10 +23,14 @@ entries = [
     }
 ]
 
-entry = {
-    "title": "Howdy",
-    "description": "Le meilleur personne"
-}
+users = [
+    {
+        "id": 1,
+        "username": "ramon",
+        "email": "ramonomondi@gmail.com",
+        "password": "1234"
+    }
+]
 
 
 @app.route('/api/v1/entries', methods=['GET'])
@@ -50,8 +55,12 @@ def get_single_entry(entry_id):
 def create_entry():
     """Creates a single entry"""
     
-    if not request.json or not 'title' in request.json or not 'description' in request.json:
+    if not request.json:
         abort(400)
+    elif not 'title' in request.json:
+        return jsonify({'message' : 'Title is required'}), 400
+    elif not 'description' in request.json:
+        return jsonify({'message' : 'Description is required'}), 400
     
     entry = {
         "id": entries[-1]['id'] + 1,
@@ -92,4 +101,47 @@ def delete_entry(entry_id):
 
     return jsonify({'message' : 'Entry deleted successfully'})
 
- 
+
+@app.route('/auth/signup', methods=['POST'])
+def signup():
+    """Creates a user"""
+
+    if not request.json:
+        abort(400)
+    elif not 'username' in request.json:
+        return jsonify({'message' : 'Username is required'}), 400
+    elif not 'email' in request.json:
+        return jsonify({'message' : 'Email is required'}), 400
+    elif not 'password' in request.json:
+        return jsonify({'message' : 'Password is required'}), 401
+
+        
+    user = {
+        "id": users[-1]['id'] + 1,
+        "username": request.json['username'],
+        "email": request.json['email'],
+        "password": request.json['password']
+    }
+
+    users.append(user)
+
+    return jsonify({'User' : user, 'message' : 'User created successfully'}), 200
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    """Logs a user into the system"""
+    
+    if not request.json:
+        abort(400)
+    elif not 'username' in request.json:
+        return jsonify({'message' : 'Username is required'}), 400
+    elif not 'password' in request.json:
+        return jsonify({'message' : 'Password is required'}), 401
+
+    for user in users:
+        if request.json['username'] == user['username']:
+            user = user
+            
+        if request.json['password'] == user['password']:
+            return jsonify({'message' : 'Login successfull'})
+   
