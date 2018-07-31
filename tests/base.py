0b6077f2b9abc19354b0c5d2d34694f5fbb02d09
Ian Otieno <ramonomondi@gmail.com>
2018-07-31 22:04:16 +0300

    Update the tearDown() method to work when testing

--- tests/base.py
+++ tests/base.py
@@ -3,28 +3,25 @@ import unittest
 import json
 from unittest import TestCase
 from app.app import app
-from app.models import theDatabase
+from app.models import Database
 from instance.config import app_config
 
 
 class BaseTestClass(TestCase):
     """Configuring the base test class for all the test cases"""
 
-
     @classmethod
     def setUp(self):
         """App configuration setup"""
         
-        self.app = app
-        self.client = app.test_client()
-        app.config.from_object(app_config['testing'])
+        self.app = app.config.from_object(app_config['testing'])
+        self.client = app.test_client()        
 
         self.user_details = {
-            'username' : 'ramon',
+            'username' : 'ramonomondi',
             'email' : 'ramonomondi@gmail.com',
-            'password' : '1234'
+            'password' : '123456789'
         }
-
 
         self.user_no_username = {
             'email' : 'torivega@wawa.com',
@@ -32,61 +29,57 @@ class BaseTestClass(TestCase):
         }
 
         self.user_no_email = {
-            'username' : 'ramon',
-            'password' : '1234'
+            'username' : 'ramonomondi',
+            'password' : '123456789'
         }
 
         self.user_no_password = {
-            'username' : 'ramon',
+            'username' : 'ramonomondi',
             'email' : 'ramonomondi@gmail.com'
         }
 
         self.user_invalid_password = {
-            'username' : 'ramon',
+            'username' : 'ramonomondi',
             'email' : 'ramonomondi@gmail.com',
-            'password' : '1209'
+            'password' : '12098899'
         }
 
-        self.user_invalid_username = {
+        self.user_wrong_username = {
             'username' : 'ramoon',
             'email' : 'ramonomondi@gmail.com',
-            'password' : '1234'
+            'password' : '123456789'
         }
+
+
 
         self.entry_contents = {
             'title' : 'Trip to oblivion',
             'description' : 'Best days of my life'
         }
 
-
         self.entry_no_title = {
             'description' : 'This is it'
         }
-
 
         self.entry_no_description = {
             'title' : 'You are in for a treat'
         }
 
-
-    def signup(self):
+    def signup_user(self):
         response =self.client.post('/auth/signup',
                         data=json.dumps(self.user_details),
                         headers={'content-type':'application/json'})
         return response
 
-    def login(self):
+    def login_user(self):
         response = self.client.post('/auth/login',
                         data=json.dumps(self.user_details),
                         headers={'content-type':'application/json'})
         return response             
-
-
-    @classmethod
-    def classTearDown(cls):
-        theDatabase().drop_user_table()
-        theDatabase().drop_entry_table()
-
+ 
+    def tearDown(self):
+        Database().drop_user_table()
+        Database().drop_entry_table()
 
 if __name__ == '__main__':
     unittest.main()
