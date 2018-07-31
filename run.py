760caf6c30fc06bb91872432e64e021039b78e46
Ian Otieno <ramonomondi@gmail.com>
2018-07-31 22:03:23 +0300

    Update to access environment variables

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app.app import app
 from instance.config import app_config
+import os
 
 
-
-app.config.from_object(app_config['development'])
+app.config.from_object(app_config[os.getenv('APP_SETTINGS')])
 
 if __name__ == '__main__':
     app.run()
