054073c2f48562964db80cba7940672ed86ceb0a
Ian Otieno <ramonomondi@gmail.com>
2018-08-03 10:31:32 +0300

    Add api.js to handle interraction with the ui

---
+++ feature/static/js/api.js
@@ -0,0 +1,61 @@
+const registerUser = () => {
+    fetch('http://127.0.0.1:5000/auth/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        confirm_password: document.getElementById('confirm_password').value
+      }),
+      headers: {
+        'Content-type': 'application/json;'
+      }
+      })
+      .then(response => response.json())
+      .then(data => {
+        if(data.message === "User created successfully"){
+            window.location.href = "./login.html";
+        }else{
+            document.getElementById('error-message').innerHTML = data.message;
+        }
+      })
+  }
+  
+const loginUser = () => {
+    fetch('http://127.0.0.1:5000/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+      }),
+      headers: {
+        'Content-type' : 'application/json;'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.message === "Login successfull"){
+            window.location.href = "./profile.html";
+        }else{
+            document.getElementById('error-message').innerHTML = data.message;
+        }
+    })
+  }
+ 
+  const publishEntry = () => {
+    fetch('http://127.0.0.1:5000/api/v1/entries', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: document.getElementById('title').value,
+        description: document.getElementById('description').value
+      }),
+      headers: {
+        Authorization: `Bearer `,
+        'Content-type' : 'application/json;'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.message)
+    })
+  }
