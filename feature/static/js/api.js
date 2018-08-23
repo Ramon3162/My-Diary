8be6971c920e9bd134acdccac49a9f819f3f5b5d
Ian Otieno <ramonomondi@gmail.com>
2018-08-23 20:49:20 +0300

    Refactor code to make it more readable

--- feature/static/js/api.js
+++ feature/static/js/api.js
@@ -16,117 +16,117 @@ const destroyEntrySession = () => {
   sessionStorage.removeItem("date");
 }
 
-// const registerUser = () => {
-//     fetch(signupUrl, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         username: document.getElementById('username').value,
-//         email: document.getElementById('email').value,
-//         password: document.getElementById('password').value,
-//         confirm_password: document.getElementById('confirm_password').value
-//       }),
-//       headers: {
-//         'Content-type': 'application/json;'
-//       }
-//       })
-//       .then(response => response.json())
-//       .then(registerData => {
-//         if(registerData.message === "User created successfully"){
-//           window.location.href = "./login.html";
-//         }else{
-//             document.getElementById('error-message').innerHTML = registerData.message;
-//         }
-//       })
-//   }
+const registerUser = () => {
+  fetch(signupUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      username: document.getElementById('username').value,
+      email: document.getElementById('email').value,
+      password: document.getElementById('password').value,
+      confirm_password: document.getElementById('confirm_password').value
+    }),
+    headers: {
+      'Content-type': 'application/json;'
+    }
+    })
+    .then(response => response.json())
+    .then(registerData => {
+      if(registerData.message === "User created successfully"){
+        window.location.href = "./login.html";
+      }else{
+          document.getElementById('error-message').innerHTML = registerData.message;
+      }
+  })
+}
   
-// const loginUser = () => {
-//   fetch(loginUrl, {
-//     method: 'POST',
-//     body: JSON.stringify({
-//       username: document.getElementById('username').value,
-//       password: document.getElementById('password').value
-//     }),
-//     headers: {
-//       'Content-type' : 'application/json;'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(loginData => {
-//       if(loginData.message === "Login successfull"){
-//           window.location.href = "./profile.html";
-//           sessionStorage.setItem("token",loginData.token);
-//       }else{
-//           document.getElementById('error-message').innerHTML = loginData.message;
-//       }
-//   })
-// }
+const loginUser = () => {
+  fetch(loginUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value
+    }),
+    headers: {
+      'Content-type' : 'application/json;'
+    }
+  })
+  .then(response => response.json())
+  .then(loginData => {
+      if(loginData.message === "Login successfull"){
+          window.location.href = "./profile.html";
+          sessionStorage.setItem("token",loginData.token);
+      }else{
+          document.getElementById('error-message').innerHTML = loginData.message;
+      }
+  })
+}
 
-// const getEntries = () => {
-//   fetch(entriesUrl, {
-//     headers: {
-//       'Authorization' : `Bearer ${sessionStorage.getItem("token")}`,
-//       'Content-type' : 'application/json;'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(entriesData => {
-//     if(entriesData.message === "All entries found successfully"){
-//       let i;
-//       for(i = 0; i < entriesData.Entries.length; i++){
-//         console.log(entriesData.Entries.length);
-//         sessionStorage.setItem((i+1), entriesData.Entries[i].id);
-//         document.getElementById('entry-data').innerHTML += `
-//         <tr>
-//           <td></td>
-//           <td><a href="javascript:void(0);" onclick="getSingleTableEntry(this)">
-//             ${entriesData.Entries[i].title}</a>
-//           </td>
-//           <td>${entriesData.Entries[i].date_posted}</td>
-//           <td><a href="javascript:void(0);" id="edit-icons" onclick="editTableEntry(this)">
-//               <i class="fa fa-pencil"></i></a>
-//           </td>
-//           <td><a href="javascript:void(0);" id="edit-icons" onclick="deleteTableEntry(this)">
-//               <i class="fa fa-trash""></i></a>
-//           </td>
-//         </tr>`
-//       }
-//       console.log(entriesData.message);
-//     }else{
-//       let table = document.getElementById('entry-data');
-//       table.style.display = "none";
-//       document.getElementById('message').innerHTML = entriesData.message;
-//     }
-//   })
-// }
- 
-// const publishEntry = () => {
-//   fetch(entriesUrl, {
-//     method: 'POST',
-//     body: JSON.stringify({
-//       title: document.getElementById('title').value,
-//       description: document.getElementById('description').value
-//     }),
-//     headers: {
-//       'Authorization' : `Bearer ${sessionStorage.getItem("token")}`,
-//       'Content-type' : 'application/json;'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     if(data.message === "Entry created successfully"){
-//       window.location.href = "./entry.html";
-//       console.log(data.message);
-//       let id = data.Entry.id;
-//       let title = data.Entry.title;
-//       let desc = data.Entry.description;
-//       let date = data.Entry.date_posted;
-//       destroyEntrySession();
-//       setEntrySession(id, title, desc, date);
-//     }else{
-//       document.getElementById('message').innerHTML = data.message;
-//     }
-//   })
-// }
+const getEntries = () => {
+  fetch(entriesUrl, {
+    headers: {
+      'Authorization' : `Bearer ${sessionStorage.getItem("token")}`,
+      'Content-type' : 'application/json;'
+    }
+  })
+  .then(response => response.json())
+  .then(entriesData => {
+    if(entriesData.message === "All entries found successfully"){
+      let i;
+      for(i = 0; i < entriesData.Entries.length; i++){
+        console.log(entriesData.Entries.length);
+        sessionStorage.setItem((i+1), entriesData.Entries[i].id);
+        document.getElementById('entry-data').innerHTML += `
+        <tr>
+          <td></td>
+          <td><a href="javascript:void(0);" onclick="getSingleTableEntry(this)">
+            ${entriesData.Entries[i].title}</a>
+          </td>
+          <td>${entriesData.Entries[i].date_posted}</td>
+          <td><a href="javascript:void(0);" id="edit-icons" onclick="editTableEntry(this)">
+              <i class="fa fa-pencil"></i></a>
+          </td>
+          <td><a href="javascript:void(0);" id="edit-icons" onclick="deleteTableEntry(this)">
+              <i class="fa fa-trash""></i></a>
+          </td>
+        </tr>`
+      }
+      console.log(entriesData.message);
+    }else{
+      let table = document.getElementById('entry-data');
+      table.style.display = "none";
+      document.getElementById('message').innerHTML = entriesData.message;
+    }
+  })
+}
+
+const publishEntry = () => {
+  fetch(entriesUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: document.getElementById('title').value,
+      description: document.getElementById('description').value
+    }),
+    headers: {
+      'Authorization' : `Bearer ${sessionStorage.getItem("token")}`,
+      'Content-type' : 'application/json;'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.message === "Entry created successfully"){
+      window.location.href = "./entry.html";
+      console.log(data.message);
+      let id = data.Entry.id;
+      let title = data.Entry.title;
+      let desc = data.Entry.description;
+      let date = data.Entry.date_posted;
+      destroyEntrySession();
+      setEntrySession(id, title, desc, date);
+    }else{
+      document.getElementById('message').innerHTML = data.message;
+    }
+  })
+}
 
 const deleteEntry = (entryId) => {
   let confirmation = confirm("Are you sure you want to delete this entry?");
