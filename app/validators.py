32d6b5b98f9feaae469be43dd2ae602c19c9c7a7
Ian Otieno <ramonomondi@gmail.com>
2018-08-29 00:45:47 +0300

    Update validators.py

--- app/validators.py
+++ app/validators.py
@@ -1,6 +1,8 @@
+import re
 from flask import jsonify
 
 def validate_entry_inputs(data):
+    print(data)
     if not data:
         return jsonify({'message' : 'Error 400. Request needs to be in JSON format.'}), 400
     elif not 'title' in data:
@@ -11,5 +13,18 @@ def validate_entry_inputs(data):
         return jsonify({'message' : 'Entry title cannot be empty.'}), 400
     elif len(data['description'].strip(" ")) < 1:
         return jsonify({'message' : 'Entry description cannot be empty.'}), 400
+# def validate_entry_inputs(data):
+#     print(data)
+#     for key in data:
+#         name = re.sub(r'\s+', '', data[key])
+#         # print(name)
+#         if not name:
+#             return jsonify({'message'+ ':' + key + ' is required'}), 400
 
-    print(data)
+# def entry_validator(data):
+#     title = data.get('title')
+#     description = data.get('description')
+
+#     data = {'title' : title, 'description' : description}
+#     validate_entry_inputs(data)
+#     return data
