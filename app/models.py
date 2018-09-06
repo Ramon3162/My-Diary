a0211bcac861f7a7f9ae1890b5db76e9e5a0c13b
KE-C02SY35BGY25 <ke-c02sy35bgy25@Ians-MacBook-Pro.local>
2018-09-06 21:56:22 +0300

    Fix pylint issues

--- app/models.py
+++ app/models.py
@@ -30,14 +30,15 @@ class User(Database):
         if not username_data:
             if not email_data:
                 self.cursor.execute("""INSERT INTO diary_users (username, password, email, status)
-                                    VALUES (%(username)s, %(password)s, %(email)s, %(status)s)""", user_data)
+                                    VALUES (%(username)s, %(password)s, %(email)s, %(status)s)""",
+                                    user_data)
                 self.conn.commit()
                 self.cursor.execute("SELECT * FROM diary_users WHERE username = %s", (username, ))
                 data = self.cursor.fetchone()
-                return jsonify({'User': User().display_user(data), 'message' : 'User created successfully'}), 201
+                return jsonify({'User': User().display_user(data),
+                                'message' : 'User created successfully'}), 201
             return jsonify({'message' : 'Email already exists'}), 400
         return jsonify({'message' : 'Username already exists'}), 400
-    
     def login(self, username, password):
         """User login"""
         credentials = request.get_json()
@@ -57,7 +58,9 @@ class User(Database):
                 self.cursor.execute("SELECT * FROM diary_users WHERE username = %s", (username, ))
                 data = self.cursor.fetchone()
                 if data:
-                    return jsonify({'token' : access_token, 'User' :User().display_user(data) , 'message':'Login successfull'}), 200
+                    return jsonify({'token' : access_token,
+                                    'User' :User().display_user(data),
+                                    'message':'Login successfull'}), 200
             return jsonify({'message':'Password is invalid'}), 400
         return jsonify({'message' : 'Username is invalid'}), 400
 
@@ -66,27 +69,30 @@ class User(Database):
         self.cursor.execute("SELECT * FROM diary_users WHERE user_id = %s", (user_id, ))
         data = self.cursor.fetchone()
         if data:
-            return jsonify({'User' : User().display_user(data), 'message' : 'User retrieved successfully'}), 200
+            return jsonify({'User' : User().display_user(data),
+                            'message' : 'User retrieved successfully'}), 200
         return jsonify({'message' : 'User not found'})
-    
     def update_user_data(self, user_id, username, email, status):
         """Method to update user data"""
         self.cursor.execute("SELECT * FROM diary_users WHERE user_id = %s", (user_id, ))
         data = self.cursor.fetchone()
         if data:
             self.cursor.execute("""UPDATE diary_users set username = %s, email = %s,
-                                status = %s WHERE user_id = %s""", (username, email, status, user_id, ))
+                                status = %s WHERE user_id = %s""",
+                                (username, email, status, user_id, ))
             self.conn.commit()
             self.cursor.execute("""SELECT * FROM diary_users WHERE user_id = %s""",
                                 (user_id, ))
             updated_data = self.cursor.fetchone()
-            return jsonify({'User' : User().display_user(updated_data), 'message' : 'User data updated successfully'}), 200
+            return jsonify({'User' : User().display_user(updated_data),
+                            'message' : 'User data updated successfully'}), 200
         return jsonify({'message' : 'User not found'})
 
 
-    def display_user(self,user):
+    def display_user(self, user):
         """Dictionary to hold entry data"""
-        self.cursor.execute("""SELECT COUNT(entry_title) FROM diary_entries WHERE id = %s""", (user[0], ))
+        self.cursor.execute("""SELECT COUNT(entry_title) FROM diary_entries WHERE id = %s""",
+                            (user[0], ))
         entry_data = self.cursor.fetchone()
         return dict(
             user_id=user[0],
@@ -106,18 +112,23 @@ class Entry(Database):
         if result:
             return jsonify({'message' : 'You cannot publish a duplicate entry.'}), 400
         self.cursor.execute("""INSERT INTO diary_entries (id, entry_title, description, date_posted)
-                            VALUES (%s, %s, %s, %s)""", (current_user, title, description, date_posted,))
+                            VALUES (%s, %s, %s, %s)""",
+                            (current_user, title, description, date_posted,))
         self.conn.commit()
-        self.cursor.execute("""SELECT * FROM diary_entries WHERE id = %s AND entry_id = (SELECT MAX(entry_id) FROM diary_entries)""", (current_user,))
+        self.cursor.execute("""SELECT * FROM diary_entries WHERE id = %s AND 
+                            entry_id = (SELECT MAX(entry_id) FROM diary_entries)""",
+                            (current_user,))
         data = self.cursor.fetchone()
-        return jsonify({'Entry': Entry().display_entry(data), 'message' : 'Entry created successfully'}), 201
+        return jsonify({'Entry': Entry().display_entry(data),
+                        'message' : 'Entry created successfully'}), 201
     def get_one_entry(self, entry_id, current_user):
         """Allows for viewing of one diary entry"""
         self.cursor.execute("""SELECT * FROM diary_entries WHERE entry_id = %s AND id = %s""",
                             (entry_id, current_user, ))
         data = self.cursor.fetchone()
         if data:
-            return jsonify({'Entry' : Entry().display_entry(data), 'message' : 'Entry retrieved successfully'}), 200
+            return jsonify({'Entry' : Entry().display_entry(data),
+                            'message' : 'Entry retrieved successfully'}), 200
         return jsonify({'message' : 'Entry not found'})
 
     def get_all_entries(self, current_user):
@@ -125,7 +136,8 @@ class Entry(Database):
         self.cursor.execute("""SELECT * FROM diary_entries WHERE id = %s""", (current_user,))
         data = self.cursor.fetchall()
         if data:
-            return jsonify({'Entries' : [Entry().display_entry(entry) for entry in data], 'message' : 'All entries found successfully'})
+            return jsonify({'Entries' : [Entry().display_entry(entry) for entry in data],
+                            'message' : 'All entries found successfully'})
         return jsonify({'message': 'No entries found'})
 
     def update_entry(self, entry_id, title, description, current_user):
@@ -136,11 +148,13 @@ class Entry(Database):
         data = self.cursor.fetchall()
         if data:
             self.cursor.execute("""UPDATE diary_entries set entry_title = %s,
-                                description = %s WHERE entry_id = %s""", (title, description, entry_id, ))
+                                description = %s WHERE entry_id = %s""",
+                                (title, description, entry_id, ))
             self.conn.commit()
             self.cursor.execute("""SELECT * FROM diary_entries WHERE entry_id = %s""", (entry_id, ))
             updated_data = self.cursor.fetchone()
-            return jsonify({'Entry' : Entry().display_entry(updated_data), 'message' : 'Entry updated successfully'}), 200
+            return jsonify({'Entry' : Entry().display_entry(updated_data),
+                            'message' : 'Entry updated successfully'}), 200
         return jsonify({'message' : 'Entry not found'})
 
     def delete_entry(self, entry_id, current_user):
@@ -155,7 +169,7 @@ class Entry(Database):
             return jsonify({'message' : 'Entry deleted successfully'}), 200
         return jsonify({'message' : 'Entry not found.'}), 400
 
-    def display_entry(self,entry):
+    def display_entry(self, entry):
         """Dictionary to hold entry data"""
         return dict(
             id=entry[0],
